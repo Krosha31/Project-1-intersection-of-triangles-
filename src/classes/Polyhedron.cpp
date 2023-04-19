308fc135b7a9f3e71ae951b3e5f089347607a74e
gis27 <gis2702@mail.ru>
2023-04-20 01:31:19 +0300

    Add operator <<

--- src/classes/Polyhedron.cpp
+++ src/classes/Polyhedron.cpp
@@ -14,3 +14,58 @@ Polyhedron::Polyhedron(const std::vector<Point<3>> &points, const std::vector<st
     this->points = points;
     this->edges = edges;
 }
+
+Polyhedron &Polyhedron::operator=(const Polyhedron &poly) {
+    points = poly.points;
+    edges = poly.edges;
+    return *this;
+}
+
+Polyhedron &Polyhedron::operator=(Polyhedron &&poly) noexcept {
+    if (this == &poly)
+        return *this;
+    points = std::move(poly.points);
+    edges = std::move(poly.edges);
+    return *this;
+}
+
+std::ostream &operator<<(std::ostream &out, const Polyhedron &poly) {
+    if (poly.points.empty()) {
+        out << "Empty\n";
+    } else {
+        for (Point<3> p: poly.points) {
+            out << p.x << ' ' << p.y << ' ' << p.z << '\n';
+        }
+        out << '\n';
+        for (int i = 0; i < poly.points.size(); ++i) {
+            size_t k = 0;
+            for (int j = 0; j < poly.points.size(); ++j) {
+                if (k < poly.edges[i].size() and poly.edges[i][k] == j) {
+                    out << '1';
+                    ++k;
+                } else {
+                    out << '0';
+                }
+
+            }
+            out << '\n';
+        }
+    }
+    return out;
+}
+
+std::istream &operator>>(std::istream &in, Polyhedron &poly) {
+    std::string s_poly;
+    in >> s_poly;
+    if (s_poly.empty()) {
+        poly = Polyhedron();
+    } else {
+        std::vector<Point<3>> points{};
+        std::vector<std::vector<bool>> edges{};
+        /*
+        Create polyhedron ticket
+        */
+        poly = Polyhedron(points, edges);
+    }
+    return in;
+}
--- src/classes/Polyhedron.h
+++ src/classes/Polyhedron.h
@@ -2,27 +2,40 @@
 #define POLYHEDRON_H
 
 #include <vector>
+#include <iostream>
 #include "Point.h"
 
 template<size_t T>
-class Point{
-
+class Point {
+public:
+    int x = 0, y = 0, z = 0;;
 };
 
 
 class Polyhedron {
 private:
     std::vector<Point<3>> points{};
-    std::vector<std::vector<bool>> edges{}; //-> chmod
-public:
-    Polyhedron() = default;
+
+    std::vector<std::vector<bool>> edges{}; //-> chmod idea
 
     explicit Polyhedron(const std::vector<Point<3>> &points, const std::vector<std::vector<bool>> &edges);
+
+public:
+    Polyhedron() = default;
 
     Polyhedron(const Polyhedron &poly);
 
     Polyhedron(Polyhedron &&poly) noexcept;
 
+    Polyhedron &operator=(const Polyhedron &poly);
+
+    Polyhedron &operator=(Polyhedron &&poly) noexcept;
+
+    friend std::ostream &operator<<(std::ostream &out, const Polyhedron &poly);
+
+    friend std::istream &operator>>(std::istream &in, Polyhedron &poly);
+
+    ~Polyhedron() = default;
 };
 
 #endif
