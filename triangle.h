56516bdc02439ffede8d6104bf86c04894d81326
Татьяна Федотова <fedotova0203@yandex.ru>
2023-05-07 22:53:53 +0300

    Corrected base classes

--- triangle.h
+++ triangle.h
@@ -1,51 +1,92 @@
-#pragma once
+#ifndef TRIANGLE_H
+#define TRIANGLE_H
 
 #include<vector>
 #include<iostream>
 #include<initializer_list>
 #include<cmath>
-#include<set>
-
-const double EPS = 1e-9;
-
-template <size_t point_size>
-class Point {
-    const size_t size_ = point_size;
-public:
-    double x_;
-    double y_;
-    double z_;
-    bool exist_;
-    Point() : x_(0), y_(0), z_(0), exist_(0) {}
-    Point(double x, double y, double z=0, bool exist=1) : x_(x), y_(y), z_(z), exist_(exist) {}
-    Point<point_size>& operator=(const Point<point_size>& other);
-    template<size_t point_size2>
-    friend std::ostream& operator<<(std::ostream& out, const Point<point_size2>& point);
-};
-
-
+#include "point.h"
 
 template<size_t point_size>
-class Triangle {
-public:
-    size_t size_ = 0;
-    std::vector <Point<point_size>> vertexe_;
-    Triangle() = default;
-    Triangle(const std::initializer_list<Point<point_size>> list) {// здесь нужно в идеале сделать сортировку, чтобы все точки располагались по часовому кругу
-        for (auto element: list) {
-            vertexe_.push_back(element);
-            size_++;
+class Triangle;
+
+template<size_t point_size>
+std::basic_ostream<char> &operator<<(std::basic_ostream<char> &out, const Triangle<point_size> &triangle) {
+    //out << "Coordinates of the triangle: \n";
+    out << "Triangle(";
+    for (int i = 0; i < 3; i++) {
+        //out << i + 1 << " point: ";
+        out << triangle.vertexe_[i];
+
+        if (i != 2) {
+            out << ", ";
         }
     }
-    template<size_t point_size2>
-    friend std::ostream& operator<<(std::ostream& out, const Triangle<point_size2>& triangle);
+    out << ")\n";
+    return out;
+}
+
+template<size_t point_size>
+bool is_triangle(const Triangle<point_size> &tr) {
+    double square_triangle = tr.vertexe_[0].get_coord(0) * (tr.vertexe_[1].get_coord(1) - tr.vertexe_[2].get_coord(1)) + tr.vertexe_[1].get_coord(0) * (tr.vertexe_[2].get_coord(1) - tr.vertexe_[0].get_coord(1)) +
+            tr.vertexe_[2].get_coord(0) * (tr.vertexe_[0].get_coord(1) - tr.vertexe_[1].get_coord(1));
+    return (square_triangle != 0.0);
+}
+
+template<size_t point_size>
+bool triangle_intersection(const Triangle<point_size> &tr1, const Triangle<point_size> &tr2) {
+    Point<point_size> intersection_point;
+    std::vector<Point<point_size>> all_points;
+    if (!(is_triangle(tr1)) || !(is_triangle(tr2))) {
+        std::cout << "This is not a triangle.";
+        return false;
+    }
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            intersection_point = lines_intersection(tr1.vertexe_[i], tr1.vertexe_[(i + 1) % 3], tr2.vertexe_[j], tr2.vertexe_[(j + 1) % 3]);
+            if (intersection_point.is_exist()) {
+                all_points.push_back(intersection_point);
+            }
+
+        }
+    }
+    if (all_points.empty())
+        return false;
+    for (int i = 0; i < all_points.size(); i++) {
+        std::cout << all_points[i];
+    }
+    return true;
+}
+
+//сделал в треугольнике 3 точки
+template<size_t point_size>
+class Triangle {
+private:
+    std::vector<Point<point_size>> vertexe_{};
+public:
+    friend bool is_triangle<point_size>(const Triangle &tr);
+
+    friend bool triangle_intersection<point_size>(const Triangle &tr1, const Triangle &tr2);
+
+    Triangle();
+
+    Triangle(const Triangle<point_size> &rhs) = default;
+
+    Triangle(Triangle<point_size> &&rhs) noexcept;
+
+    Triangle(const std::initializer_list<Point<point_size>> &list);// здесь нужно в идеале сделать сортировку, чтобы все точки располагались по часовому круг
+
+    Triangle<point_size> &operator=(const Triangle<point_size> &rhs) = default;
+
+    Triangle<point_size> &operator=(Triangle<point_size> &&rhs) noexcept;
+
+    friend std::basic_ostream<char> &operator<<<point_size>(std::basic_ostream<char> &out,const Triangle &triangle);
+
+    Point<point_size> get_point(size_t pos) const;
+
+    void set_point(size_t pos, const Point<point_size> &point);
+
+    ~Triangle() = default;
 };
 
-template<size_t point_size>
-bool is_triangle(const Triangle<point_size>& tr);
-
-template<size_t point_size>
-Point<point_size> lines_intersection(Point<point_size> p1, Point<point_size> p2, Point<point_size> p3, Point<point_size> p4);
-
-template<size_t point_size>
-bool triangle_intersection(const Triangle<point_size>& tr1, const Triangle<point_size>& tr2);
+#endif
